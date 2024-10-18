7cee0c369f093c6aad3c49010eb9983ae267bf69
rawleyrudel <rudelra@vcu.edu>
2024-10-18 15:59:42 -0400

    update

--- RhythmPartnerGame/Assets/Scripts/GameManager.cs
+++ RhythmPartnerGame/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -16,10 +17,22 @@ public class GameManager : MonoBehaviour
 
     public int scorePerNote = 100;
 
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierThresholds;
+
+
+    public Text scoreText;
+
+    public Text multiText;
+
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+
+        scoreText.text = "Score: 0";
+        currentMultiplier = 1;
     }
 
     // Update is called once per frame
@@ -41,12 +54,31 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log("Hit On Time");
 
-        currentScore += scorePerNote;
+        if (currentMultiplier - 1 < multiplierThresholds.Length)
+        {
+            multiplierTracker++;
+
+            if (multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+            }
+
+        }
+        multiText.text = "Multiplier: x" + currentMultiplier;
+
+        currentScore += scorePerNote * currentMultiplier;
+        scoreText.text = "Score: " + currentScore;
     }
 
     public void NoteMissed()
     {
         Debug.Log("Missed Note");
+
+        currentMultiplier = 1;
+        multiplierTracker = 0;
+
+        multiText.text = "Multiplier: x" + currentMultiplier;
     }
 
 
