02e1a9024216548532aa8db83de432ad90aeeac5
rawleyrudel <rudelra@vcu.edu>
2024-10-20 10:19:58 -0400

    hitfeedback

--- RhythmPartnerGame/Assets/Scripts/GameManager.cs
+++ RhythmPartnerGame/Assets/Scripts/GameManager.cs
@@ -16,6 +16,8 @@ public class GameManager : MonoBehaviour
     public int currentScore;
 
     public int scorePerNote = 100;
+    public int scorePerGoodNote = 125;
+    public int scorePerPerfectNote = 150;
 
     public int currentMultiplier;
     public int multiplierTracker;
@@ -38,23 +40,24 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!startPlaying)
+        if (!startPlaying)
         {
-            if(Input.anyKeyDown)
+            if (Input.anyKeyDown)
             {
                 startPlaying = true;
                 theBS.hasStarted = true;
 
                 theMusic.Play();
             }
-        }    
+        }
     }
 
     public void NoteHit()
     {
         Debug.Log("Hit On Time");
 
-        if (currentMultiplier - 1 < multiplierThresholds.Length)
+        // Check that currentMultiplier does not exceed the bounds of multiplierThresholds array
+        if (currentMultiplier - 1 < multiplierThresholds.Length - 1)
         {
             multiplierTracker++;
 
@@ -63,12 +66,37 @@ public class GameManager : MonoBehaviour
                 multiplierTracker = 0;
                 currentMultiplier++;
             }
-
         }
+        else
+        {
+            // Cap the multiplier at the maximum possible value
+            currentMultiplier = multiplierThresholds.Length;
+        }
+
+        // Update the multiplier text
         multiText.text = "Multiplier: x" + currentMultiplier;
 
+        // Update the score based on current multiplier
         currentScore += scorePerNote * currentMultiplier;
         scoreText.text = "Score: " + currentScore;
+    }
+
+    public void NormalHit()
+    {
+        currentScore += scorePerNote * currentMultiplier;
+        NoteHit();
+    }
+
+    public void GoodHit()
+    {
+        currentScore += scorePerGoodNote * currentMultiplier;
+        NoteHit();
+    }
+
+    public void PerfectHit()
+    {
+        currentScore += scorePerPerfectNote * currentMultiplier;
+        NoteHit();
     }
 
     public void NoteMissed()
@@ -80,8 +108,6 @@ public class GameManager : MonoBehaviour
 
         multiText.text = "Multiplier: x" + currentMultiplier;
     }
-
-
 }
 
 
